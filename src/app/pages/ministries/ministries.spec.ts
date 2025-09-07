462f6c7797f0d905e2f9a9f60375e75eb7fec49e
Zachary Jones <joneszj@gmail.com>
2025-09-07 16:08:41 -0400

    Init

---
+++ src/app/pages/ministries/ministries.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Ministries } from './ministries';
+
+describe('Ministries', () => {
+  let component: Ministries;
+  let fixture: ComponentFixture<Ministries>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Ministries]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Ministries);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/ministries/ministries.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ministries',
+  imports: [],
+  templateUrl: './ministries.html',
+  styleUrl: './ministries.css'
+})
+export class Ministries {
+
+}
